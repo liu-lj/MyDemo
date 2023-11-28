20e5e7b350a03baaaf0f53928a84a699b4af9a92
callg <34684397+callG@users.noreply.github.com>
2023-11-28 21:02:52 +0800

    初始化工程和场景

---
+++ Source/TankDemo/TankDemoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankDemoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATankDemoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATankDemoGameMode();
+};
+
+
+
